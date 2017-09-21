1e5366ab4b563d8af861ba93c2a508cd880014a6
Carlos Ribeiro <chdribeiro@hotmail.com>
2017-09-21 16:36:49 -0300

    Update Final - Falta Grafico

--- Lanchonete/Menu.cs
+++ Lanchonete/Menu.cs
@@ -12,13 +12,16 @@ namespace Lanchonete
 
         public static void Logo()
         {
+            
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(@"                 _                   _                                 ");
             Console.WriteLine(@" _ __   ___   __| |_ __ __ _  ___   | |__  _   _  __ _ _   _  ___ _ __ ");
             Console.WriteLine(@"| '_ \ / _ \ / _` | '__/ _` |/ _ \  | '_ \| | | |/ _` | | | |/ _ \ '__|");
             Console.WriteLine(@"| |_) | (_) | (_| | | | (_| | (_) | | |_) | |_| | (_| | |_| |  __/ |   ");
-            Console.WriteLine(@"| .__/ \___/ \__,_|_|  \__,_|\___/  |_.__/ \__,_|\__, |\__,_|\___|_|  ");
+            Console.WriteLine(@"| .__/ \___/ \__,_|_|  \__,_|\___/  |_.__/ \__,_|\__, |\__,_|\___|_|   ");
             Console.WriteLine(@"|_|                                              |___/                 ");
             Console.WriteLine("\n\n");
+            Console.ResetColor();
         }
 
         public static void MenuInicial()
